7a9828698555cd625409635f484492ec8f995f74
Akshay Makkar <akshaym1103@gmail.com>
2019-06-07 16:48:47 +0530

    Ground floor Room nos. added

---
+++ Navigate NCU/Assets/Scripts/option4.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class option4 : MonoBehaviour
+{
+    public GameObject img,arrow,back;
+    public GameObject menu;
+
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        back.SetActive(true);
+        img.SetActive(true);
+        arrow.SetActive(true);
+        menu.SetActive(false);
+    }
+}
---
+++ Navigate NCU/Assets/Scripts/option5.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class option5 : MonoBehaviour
+{
+    public GameObject img,arrow,back;
+    public GameObject menu;
+
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        back.SetActive(true);
+        img.SetActive(true);
+        arrow.SetActive(true);
+        menu.SetActive(false);
+    }
+}
