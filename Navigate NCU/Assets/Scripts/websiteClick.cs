837f04b70e2c61c6417cb61a0be330d4fe353cdd
Akshay Makkar <akshaym1103@gmail.com>
2019-06-09 20:41:49 +0530

    .

---
+++ Navigate NCU/Assets/Scripts/websiteClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class websiteClick : MonoBehaviour
+{
+    public GameObject layers;
+    void OnMouseDown()
+    {
+        Debug.Log("clicked");
+        gameObject.SetActive(false);
+        layers.SetActive(true);
+    }
+}
